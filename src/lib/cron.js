01bae33f48822f791b28656b6bbe9ec121659f80
IT21363702 <99170248+IT21363702@users.noreply.github.com>
2025-08-25 20:38:27 +0530

    Fix cron job schedule syntax and add newline at end of file

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const Job = new cron.CronJob("*/14***" , function(){
+const Job = new cron.CronJob("*/14 * * * *", function(){
     https
         .get("https://BOOKWARM-APP.com.onrender/api/register" , (res) =>{
             if(res.statusCode === 200){
@@ -16,3 +16,5 @@ const Job = new cron.CronJob("*/14***" , function(){
 });
 
 export default Job;
+
+//'0 0 * * *' - Daily at midnight '0 */6 * * *' - Every 6 hours '*/30 * * * *' - Every 30 minutes '0 9 * * 1-5' - Weekdays at 9 AM
